46b6086564228c60f24d3e4f79e0c676a832f578
kimdb35 <kimdb35@naver.com>
2017-04-13 16:48:23 +0900

    hooh

--- WebContent/js/game1.js
+++ WebContent/js/game1.js
@@ -1,108 +1,58 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = 50;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var scroll_x_start = 0;
-var rightPressed = false;
-var leftPressed = false;
 var gameOver=false;
 var horseStatus=0;
-
-window.requestAnimationFrame = function() {
-
-	return window.requestAnimationFrame ||
-
-	window.webkitRequestAnimationFrame ||
-
-	mozRequestAnimationFrame ||
-
-	window.oRequestAnimationFrame ||
-
-	window.msRequestAnimationFrame ||
-
-	function (callback) {
-
-	window.setTimeout(callback, 1000);
-
-	}
-
-	}();
-
-
 
 
 var img=new Image();
 var horses=[
-	{number:1, pos:{x:0, y:canvas.height/2},source_size: {w:128, h:128},
-	    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7, rank:1  },
-	    {number:2, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-		    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-		    {number:3, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-			    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-			    {number:4, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-				    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-				    {number:5, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-					    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-					    {number:6, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-						    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-						    {number:7, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-							    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-							    {number:8, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-								    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-								    {number:9, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-									    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-									    {number:10, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-										    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-										    {number:11, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-											    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-											    {number:12, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-												    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 },
-												    {number:13, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-													    target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1 }
-	    
-
-]
+	{
+		number:1, pos:{x:0, y:canvas.height/2},source_size: {w:128, h:128},
+		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7, rank:1
+	},
+	{
+		number:2, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
+		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
+	},
+	{
+		number:3, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
+		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
+	},
+	{
+		number:4, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
+		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
+	},
+	{
+		number:5, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
+		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
+	},
+	{
+		number:6, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
+		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
+	},
+	{
+		number:7, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
+		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
+	},
+	{
+		number:8, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
+		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
+	}
+	]
 
 img.src='img/horse-black.png';
 
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
-
-function keyDownHandler(e) {
-    if(e.keyCode == 39) {
-        rightPressed = true;
-    }
-    else if(e.keyCode == 37) {
-        leftPressed = true;
-    }
-}
-function keyUpHandler(e) {
-    if(e.keyCode == 39) {
-        rightPressed = false;
-    }
-    else if(e.keyCode == 37) {
-        leftPressed = false;
-    }
-}
-function mouseMoveHandler(e) {
-	
-}
 function collisionDetection() {
 	if(!gameOver)
 	for(var idx=0; idx<horses.length; idx++){
 		if(horses[idx].x>=canvas.width*0.8){
-			alert(horses[idx].number+"번 말");
+			alert(horses[idx].number+"踰� 留�");
 			gameOver=true;
 		}
 	}
 }
 
-function drawHorse() {						// 말 그리기
+function drawHorse() {						// 留� 洹몃━湲�
 	for(var idx=0; idx<horses.length ;idx++) {
 		 ctx.beginPath();
 		    ctx.drawImage(
@@ -121,15 +71,6 @@ function drawHorse() {						// 말 그리기
 		
 	}
 }
-function drawPaddle() {
-}
-function drawBricks() {
-}
-function drawScore() {
-}
-function drawLives() {
-}
-
 function drawBackground(){
 	ctx.beginPath();
 	ctx.fillRect(0, 0, canvas.width, canvas.height/2);
@@ -149,11 +90,7 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // object drawing section
     drawBackground();
-    drawBricks();
     drawHorse();
-    drawPaddle();
-    drawScore();
-    drawLives();
     collisionDetection();
     // object position setting
     horseStatus=(horseStatus<3)?horseStatus+1:0;
