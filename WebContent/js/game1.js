f771f4d0a97f7451b4cc953d7393e5cf8a895ca6
jj <kjho1e3@naver.com>
2017-04-18 00:53:56 +0900

    뭐좀바꿧다

--- WebContent/js/game1.js
+++ WebContent/js/game1.js
@@ -1,12 +1,11 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var canvas=document.getElementById("game");
+var ctx=canvas.getContext("2d");
 var gameOver=false;
 var horseStatus=0;
 var elapsedTime=0;
 var bg_pos = {
 		x:0, y:0, swidth:1536, sheight:1536, dx:0, dy:0, dwidth:canvas.width, dheight:canvas.height
 };
-
 
 var img=new Image();
 var backImg=new Image();
@@ -30,21 +29,9 @@ var horses=[
 	{
 		number:5, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
 		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
-	},
-	{
-		number:6, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
-	},
-	{
-		number:7, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
-	},
-	{
-		number:8, pos:{x:0, y:canvas.height/2+20},source_size: {w:128, h:128},
-		target_size: {w:128, h:128}, sprite:{sprite_x:0,sprite_y:384}, dx:7 ,rank:1
 	}
-	]
-
+	];
+var rank=new Array(5);
 img.src='img/horse-black.png';
 backImg.src='img/hihi.png';
 function collisionDetection() {
@@ -76,17 +63,37 @@ function drawHorse() {						// 말 그림
 		
 	}
 }
+function drawMiniMap(){
+	ctx.beginPath();
+	ctx.fillStyle="rgba(100,100,100,0.2)";
+	ctx.arc(160, 160, 100, Math.PI*1.5, Math.PI*0.5,true);
+	ctx.fillRect(160, 60, 100, 200);
+	ctx.fill();
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.arc(260, 160, 100, Math.PI*1.5, Math.PI*0.5,false);
+	ctx.fill();
+	ctx.closePath();
+}
 function drawElapasedTime(){
 	ctx.beginPath();
 	ctx.font="30px Arial";
-	ctx.fillText("남은 거리:"+(1500-elapsedTime)+"M", 100, 100, 100);
+	ctx.fillText("남은 거리:"+(1000-elapsedTime)+"M", canvas.width-290, 80, 100);
 	ctx.closePath();
 }
 function drawScoreBoard(){
 	ctx.beginPath();
 	ctx.fillStyle="rgba(200,100,100,0.3)";
 	ctx.fillRect(canvas.width-300, 100, 200, 600);
-	ctx.fill();
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.font="24px Arial";
+	ctx.fillStyle="black";
+	ctx.fillText("1등: "+rank[0].number, canvas.width-280, 160, 100);
+	ctx.fillText("2등: "+rank[1].number, canvas.width-280, 180, 100);
+	ctx.fillText("3등: "+rank[2].number, canvas.width-280, 200, 100);
+	ctx.fillText("4등: "+rank[3].number, canvas.width-280, 220, 100);
+	ctx.fillText("5등: "+rank[4]	.number, canvas.width-280, 240, 100);
 	ctx.closePath();
 }
 function drawBackground(){
@@ -102,9 +109,8 @@ function drawBackground(){
 			bg_pos.dwidth,
 			bg_pos.dheight/2
 			);
-	ctx.fillRect(0, bg_pos.dheight/2, canvas.width, canvas.height/2)
 	ctx.fillStyle="#804000";
-	ctx.fill();
+	ctx.fillRect(0, bg_pos.dheight/2, canvas.width, canvas.height/2)
 	ctx.closePath();
 }
 
@@ -117,18 +123,24 @@ function draw() {
     drawBackground();
     drawHorse();
     drawElapasedTime();
+    drawMiniMap();
     collisionDetection();
-    drawScoreBoard();
     // object position setting
     horseStatus=(horseStatus<3)?horseStatus+1:0;
     // console.log(horseStatus);
-    var rank=new Array();
+    rank=new Array(5);
     for(var idx=0; idx<horses.length; idx++){
     	rank.push(horses[idx]);
     }
     rank.sort(function(a, b) {
-    	return b.pos.x-a.pos.x;
+    	if(a.pos.x>b.pos.x)
+    		return -1;
+    	else if(a.pos.x<b.pos.x)
+    		return 1;
+    	else
+    		return 0;
     })
+    console.log(rank);
   
     for(var idx=0; idx<horses.length; idx++){
     	horses[idx].rank=rank.indexOf(horses[idx]);
@@ -138,14 +150,13 @@ function draw() {
     		horses[idx].dx*=-1;
     	horses[idx].sprite.sprite_x=(horses[idx].target_size.w*horseStatus);  
     }
+    drawScoreBoard();
     if(bg_pos.x >= 3072) bg_pos.x =1 ;
-    else bg_pos.x += 12;
+    else bg_pos.x += 16;
     // configuration of FPS
     setTimeout(function() {
         requestAnimationFrame(draw);
-      }, 1000/30);
+      }, 1000/35);
 
 }
-
-
 draw();
